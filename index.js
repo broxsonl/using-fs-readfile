c5d98e8318b2d2c660004905330cac6551c481d6
Lee Broxson <broxsonl@gmail.com>
2016-11-27 23:10:46 -0800

    Setup for promise method.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const callbackMethod = require('./lib/callback.js');
+const promiseMethod = require('./lib/promises.js');
 
 callbackMethod(printIt);
+promiseMethod(printIt);
 
 
 function printIt(data) {
